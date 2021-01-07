6eb00cc6f5e5c16dd23070d8de7c96e981ef9d91
Yahya Naser <yahyanaser99@gmail.com>
2021-01-07 14:29:06 +0400

    covid-20

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import {  Bar } from 'react-chartjs-2';
+import { Line, Bar } from 'react-chartjs-2';
 
 import { fetchDailyData } from '../../api';
 
@@ -39,7 +39,34 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     ) : null
   );
 
-
+  // const lineChart = (
+  //   dailyData[0] ? (
+  //     <Line
+  //       data={{
+  //         labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+  //         datasets: [{
+  //           data: dailyData.map((data) => data.confirmed),
+  //           label: 'Infected',
+  //           borderColor: '#3333ff',
+  //           fill: true,
+  //         }, {
+  //           data: dailyData.map((data) => data.deaths),
+  //           label: 'Deaths',
+  //           borderColor: 'red',
+  //           backgroundColor: 'rgba(255, 0, 0, 0.5)',
+  //           fill: true,
+  //         },  {
+  //           data: dailyData.map((data) => data.recovered),
+  //           label: 'Recovered',
+  //           borderColor: 'green',
+  //           backgroundColor: 'rgba(0, 255, 0, 0.5)',
+  //           fill: true,
+  //         },
+  //         ],
+  //       }}
+  //     />
+  //   ) : null
+  // );
 
   return (
     <div className={styles.container}>
