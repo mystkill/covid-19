ccaf8abe31c6f8ce5360330c16f35345d0d4074e
Yahya Naser <yahyanaser99@gmail.com>
2021-01-07 14:34:23 +0400

    covid-19_last

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -39,38 +39,38 @@ const Chart = ({ data: { confirmed, recovered, deaths }, country }) => {
     ) : null
   );
 
-  // const lineChart = (
-  //   dailyData[0] ? (
-  //     <Line
-  //       data={{
-  //         labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
-  //         datasets: [{
-  //           data: dailyData.map((data) => data.confirmed),
-  //           label: 'Infected',
-  //           borderColor: '#3333ff',
-  //           fill: true,
-  //         }, {
-  //           data: dailyData.map((data) => data.deaths),
-  //           label: 'Deaths',
-  //           borderColor: 'red',
-  //           backgroundColor: 'rgba(255, 0, 0, 0.5)',
-  //           fill: true,
-  //         },  {
-  //           data: dailyData.map((data) => data.recovered),
-  //           label: 'Recovered',
-  //           borderColor: 'green',
-  //           backgroundColor: 'rgba(0, 255, 0, 0.5)',
-  //           fill: true,
-  //         },
-  //         ],
-  //       }}
-  //     />
-  //   ) : null
-  // );
+  const lineChart = (
+    dailyData[0] ? (
+      <Line
+        data={{
+          labels: dailyData.map(({ date }) => new Date(date).toLocaleDateString()),
+          datasets: [{
+            data: dailyData.map((data) => data.confirmed),
+            label: 'Infected',
+            borderColor: '#3333ff',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.deaths),
+            label: 'Deaths',
+            borderColor: 'red',
+            backgroundColor: 'rgba(255, 0, 0, 0.5)',
+            fill: true,
+          }, {
+            data: dailyData.map((data) => data.recovered),
+            label: 'Recovered',
+            borderColor: 'green',
+            backgroundColor: 'rgba(0, 255, 0, 0.5)',
+            fill: true,
+          },
+          ],
+        }}
+      />
+    ) : null
+  );
 
   return (
     <div className={styles.container}>
-      {country ? barChart : barChart}
+      {country ? barChart : lineChart}
     </div>
   );
 };
